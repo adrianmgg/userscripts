333ba8e7ecad85135e1e92625fd6a7ed9d8be748
adrianmgg <16143061+adrianmgg@users.noreply.github.com>
2023-04-27 12:37:00 -0700

    curseforge/mod_favicons - fix for redesign

--- curseforge.com/mod_favicons/mod_favicons.user.js
+++ curseforge.com/mod_favicons/mod_favicons.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         curseforge mod favicon
 // @namespace    https://github.com/adrianmgg
-// @version      1.0.0
+// @version      1.1.0
 // @description  use a mod's icon (or a mod category's icon) as its curseforge page's favicon
 // @author       amgg
 // @match        https://www.curseforge.com/*
+// @match        https://legacy.curseforge.com/*
 // @grant        none
 // @run-at       document-end
 // @license      MIT
@@ -33,14 +34,29 @@ function getModPageIcon() {
     }
 }
 
-(function() {
-    const faviconLink = document.querySelector('link[rel~="icon"]');
-    if(faviconLink === null) return;
-    const categoryPageIcon = getCategoryPageIcon();
-    const modPageIcon = getModPageIcon();
-    if(categoryPageIcon !== null) {
-        faviconLink.href = categoryPageIcon;
-    } else if(modPageIcon !== null) {
-        faviconLink.href = modPageIcon;
+function getIconUrl() {
+    if(window.location.hostname === 'legacy.curseforge.com') {
+        return getCategoryPageIcon() ?? getModPageIcon() ?? null;
+    } else {
+        const nextData = JSON.parse(document.getElementById('__NEXT_DATA__')?.textContent ?? '{}');
+        return nextData?.props?.pageProps?.project?.avatarUrl;
     }
+}
+
+(function() {
+    const iconUrl = getIconUrl();
+    if(iconUrl === null) return;
+    // get the current favicon elem (if any) and swap its url
+    const faviconLink = document.querySelector('link[rel~="icon"]');
+    if(faviconLink !== null) faviconLink.href = iconUrl;
+    // watch for any favicon elems added later and swap out their urls
+    (new MutationObserver(function(mutations, observer) {
+        for(const mutation of mutations) {
+            for(const node of mutation.addedNodes) {
+                if(node.nodeType === Node.ELEMENT_NODE && node.nodeName === 'LINK' && node.matches('[rel~="icon"]')) {
+                    node.href = iconUrl;
+                }
+            }
+        }
+    })).observe(document.head, {subtree: false, childList: true});
 })();
